c9b13b8e3daa7e2e1c97ae93a874fe4fd8059577
Ramya Canessane <ramyacanessane@icloud.com>
2026-06-01 11:21:09 +0200

    Initial Commit

---
+++ CarnetDeVoyageTests/CarnetDeVoyageTests.swift
@@ -0,0 +1,19 @@
+//
+//  CarnetDeVoyageTests.swift
+//  CarnetDeVoyageTests
+//
+//  Created by Canessane Ramya on 01/06/2026.
+//
+
+import Testing
+@testable import CarnetDeVoyage
+
+struct CarnetDeVoyageTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
