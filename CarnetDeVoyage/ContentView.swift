cb84d4c3811d9012f6ea8e2a761bbab96e1b4707
Ramya Canessane <ramyacanessane@icloud.com>
2026-06-01 14:28:19 +0200

    Changement couleur du texte

--- CarnetDeVoyage/ContentView.swift
+++ CarnetDeVoyage/ContentView.swift
@@ -16,6 +16,7 @@ struct ContentView: View {
                 .foregroundStyle(.tint)
             Text("Ramya Canessane")
                 .font(.largeTitle)
+                .foregroundStyle(.green)
         }
         .padding()
     }
